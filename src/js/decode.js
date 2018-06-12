07f6411a57ab978020247e91620810972f8c40e9
ming.jin01 <ming.jin01@bianlifeng.com>
2018-06-12 21:29:40 +0800

    update

--- src/js/decode.js
+++ src/js/decode.js
@@ -1,86 +1,98 @@
 !(function () {
-    function $(id) {
-        return document.getElementById(id);
-    }
-    function testUrl(url, cb) {
-        // 不好
-        // var img = new Image();
-        // img.onerror = function name(error) {
-        //     console.log(error);
-        // }
-        // img.onload = function name(e) {
-        //     console.log(e);
-        //     typeof cb === 'function' && cb();
-        // }
-        // img.src = url;
+  function $(id) {
+    return document.getElementById(id);
+  }
+  function testUrl(url, cb) {
+    // 不好
+    // var img = new Image();
+    // img.onerror = function name(error) {
+    //     console.log(error);
+    // }
+    // img.onload = function name(e) {
+    //     console.log(e);
+    //     typeof cb === 'function' && cb();
+    // }
+    // img.src = url;
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', url, true);
-        xhr.send();
-        xhr.onreadystatechange = function(e){
-            if(xhr.status < 4){
-                return;
-            }
-            if(xhr.readyState >= 200 && xhr.readyState <= 504){
-                typeof cb === 'function' && cb(xhr.responseText, e);
-            }
-        }
-        setTimeout(function(){
-            xhr.abort();
-            console.log(xhr.responseURL);
-        },50);
-        history.pushState(url,document.title, null);
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.send();
+    xhr.onreadystatechange = function (e) {
+      if (xhr.status < 4) {
+        return;
+      }
+      if (xhr.readyState >= 200 && xhr.readyState <= 504) {
+        typeof cb === 'function' && cb(xhr.responseText, e);
+      }
     }
-    function decode(val) {
-        var times = 0;
-        if (!val.match(/^(http(s)?:\/\/)?([a-zA-Z]+\.){1,}[a-zA-Z]+(\/)?(.)*/)) {
-            console.error('不合法的url');
-            return val;
-        }
-        // testUrl(val,function(e){
-        //     console.log(e);
-        // });
-        var data = {
-            params: (val.match(/[\=]/) || []).length,
-            bits: val.length,
-            length: val.length,
-            protocol: (val.match(/^http(s)?/) || ['未识别'])[0],
-            domain: val.replace(/^(http[s]?:\/\/)?(([a-zA-Z]+\.){1,}[a-zA-Z]+)(.)*/, '$2') || '',
-            // url: val
-        }
-        for (var da in data) {
-            try {
-                $('data-' + da).innerHTML = data[da];    
-            } catch (error) {
-                console.log('this dom of id is data-' + da + ' is not exzist!!');
-            }
-        }
-        resultArea.value = val;
-        if(auto.checked){
-          val = val.replace(/(\?(?!\?)|\&|\#)/g, '\n$1\n');
-        }
-        function fn(val) {
-            times++;
-            val = window.decodeURIComponent(val);
-            times > 2 ? '' : fn(val);
-            return val;
-        }
-        return fn(val);
+    setTimeout(function () {
+      xhr.abort();
+      console.log(xhr.responseURL);
+    }, 50);
+    history.pushState(url, document.title, null);
+  }
+  function decode(val) {
+    var times = 0;
+    if (!val.match(/^(http(s)?:\/\/)?[a-z0-9]+(\.[a-z0-9]+)*(\/)?(.)*/i)) {
+      console.error('不合法的url');
+      return val;
     }
-    var txt = $('url_txt'),
-        cbx = $('isClear_cbx'),
-        auto = $('isAutoChangeLine_cbx'),
-        resultArea = $('json_result'),
-        cacheValue = '';
-    txt.addEventListener('paste', function (e) {
-        setTimeout(function () {
-            cacheValue = txt.value;
-            txt.value = decode(cacheValue) || cacheValue;
-        }, 0);
-    });
-    txt.addEventListener('blur', function (e) {
-        if (cbx.checked) {
-            txt.value = '';
-        }
-    })
+    // testUrl(val,function(e){
+    //     console.log(e);
+    // });
+    var data = {
+      params: (val.match(/[\=]/) || []).length,
+      bits: val.length,
+      length: val.length,
+      protocol: (val.match(/^http(s)?/) || ['未识别'])[0],
+      domain: val.replace(/^(http[s]?:\/\/)?([a-z0-9]+(?:\.[a-z0-9]+)*)(.)*/i, '$2') || '',
+      // url: val
+    }
+    for (var da in data) {
+      try {
+        $('data-' + da).innerHTML = data[da];
+      } catch (error) {
+        console.log('this dom of id is data-' + da + ' is not exzist!!');
+      }
+    }
+    resultArea.value = val;
+    if (auto.checked) {
+      val = val.replace(/(\?(?!\?)|\&|\#)/g, '\n$1\n');
+    }
+    function fn(val) {
+      times++;
+      val = window.decodeURIComponent(val);
+      times > 2 ? '' : fn(val);
+      return val;
+    }
+    return fn(val);
+  }
+  var txt = $('url_txt'),
+    cbx = $('isClear_cbx'),
+    auto = $('isAutoChangeLine_cbx'),
+    resultArea = $('json_result'),
+    cacheValue = '';
+  txt.addEventListener('paste', function (e) {
+    setTimeout(function () {
+      cacheValue = txt.value;
+      txt.value = decode(cacheValue) || cacheValue;
+    }, 0);
+  });
+  txt.addEventListener('blur', function (e) {
+    if (cbx.checked) {
+      txt.value = '';
+    }
+  })
 })();
+
+
+window.addEventListener('keydown', function (e) {
+  console.log('keyDown', e.keyCode);
+});
+window.addEventListener('keypress', function (e) {
+  console.log('keyPress', e.keyCode);
+});
+window.addEventListener('keyup', function (e) {
+  console.log('keyUp', e.keyCode);
+});
+
--- src/js/fastParse.js
+++ src/js/fastParse.js
@@ -1,67 +1,67 @@
 onmessage = function (event) {
-    var objAim = null,
-        errorMsg = "",
-        level = 0,
-        objStr = "";
-    try {
-        objAim = JSON.parse(event.data, function (k, v) {
-            //console.log(k); // 输出当前的属性名，从而得知遍历顺序是从内向外的，
-            // 最后一个属性名会是个空字符串。
-            //{"a":1,"b":{"c":1},"d":{"e":{"f":{"g":1}}},"h":"","i":"\n1" , "k" : null }
-            return v; // 返回原始属性值，相当于没有传递 reviver 参数。
-        });
-    } catch (ex) {
-        errorMsg = ex.message;
-        console.log('json parse faile:' + ex.message);
-    }
-    function deep(obj, use_N) {
-        var len = 0;
-        level++;
-        switch (toString.call(obj)) {
-            case '[object Object]':
-                objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + "{\n";
-                for (var it in obj) { }
-                for (var item in obj) {
-                    objStr += (new Array(level + 1)).join("\t") + JSON.stringify(item) + ' : ';
-                    if (obj.hasOwnProperty(item)) {
-                        deep(obj[item]);
-                        level--;
-                        objStr += it === item ? "\n" : ",\n";
-                    }
-                }
-                objStr += (new Array(level)).join("\t") + "}";
-                break;
-            case '[object Array]':
-                objStr += "[";
-                len = obj.length;
-                obj.forEach(function (item, index) {
-                    deep(item, typeof item === 'number' || typeof item === 'string'|| typeof item === 'object');
-                    level--;
-                    objStr += (len - 1) === index ? "" : ",";
-                });
-                objStr += "\n" + (new Array(level)).join("\t") + "]";
-                break;
-            case '[object Number]':
-            case '[object Boolean]':
-                objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + obj.toString();
-                break;
-            case '[object Date]':
-            case '[object RegExp]':
-                objStr += "\"" + obj.toString() + "\"";
-                break;
-            case '[object Null]':
-            case '[object String]':
-                objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + JSON.stringify(obj);
-                break;
-            default:
-                //[object Undefined] ,[object Function] ,[object Event]
-                objStr += (obj + "");
-                break;
+  var objAim = null,
+    errorMsg = "",
+    level = 0,
+    objStr = "";
+  try {
+    objAim = JSON.parse(event.data, function (k, v) {
+      //console.log(k); // 输出当前的属性名，从而得知遍历顺序是从内向外的，
+      // 最后一个属性名会是个空字符串。
+      //{"a":1,"b":{"c":1},"d":{"e":{"f":{"g":1}}},"h":"","i":"\n1" , "k" : null }
+      return v; // 返回原始属性值，相当于没有传递 reviver 参数。
+    });
+  } catch (ex) {
+    errorMsg = ex.message;
+    console.log('json parse faile:' + ex.message);
+  }
+  function deep(obj, use_N) {
+    var len = 0;
+    level++;
+    switch (toString.call(obj)) {
+      case '[object Object]':
+        objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + "{\n";
+        for (var it in obj) { }
+        for (var item in obj) {
+          objStr += (new Array(level + 1)).join("\t") + JSON.stringify(item) + ' : ';
+          if (Object.prototype.hasOwnProperty.call(obj, item)) {
+            deep(obj[item]);
+            level--;
+            objStr += it === item ? "\n" : ",\n";
+          }
         }
+        objStr += (new Array(level)).join("\t") + "}";
+        break;
+      case '[object Array]':
+        objStr += "[";
+        len = obj.length;
+        obj.forEach(function (item, index) {
+          deep(item, typeof item === 'number' || typeof item === 'string' || typeof item === 'object');
+          level--;
+          objStr += (len - 1) === index ? "" : ",";
+        });
+        objStr += "\n" + (new Array(level)).join("\t") + "]";
+        break;
+      case '[object Number]':
+      case '[object Boolean]':
+        objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + obj.toString();
+        break;
+      case '[object Date]':
+      case '[object RegExp]':
+        objStr += "\"" + obj.toString() + "\"";
+        break;
+      case '[object Null]':
+      case '[object String]':
+        objStr += (use_N ? ("\n" + (new Array(level)).join("\t")) : '') + JSON.stringify(obj);
+        break;
+      default:
+        //[object Undefined] ,[object Function] ,[object Event]
+        objStr += (obj + "");
+        break;
     }
-    deep(objAim);
-    postMessage({ objAim: objAim, aimStr: objStr, errorMsg: errorMsg });
+  }
+  deep(objAim);
+  postMessage({ objAim: objAim, aimStr: objStr, errorMsg: errorMsg });
 };
 onerror = function (event) {
-    console.log('error form worker:'+event);
+  console.log('error form worker:' + event);
 };
